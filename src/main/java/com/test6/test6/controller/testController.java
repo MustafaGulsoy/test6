44ba05498b51dd71ac787009d7273e75355c29b4
Mustafa Gülsoy <mustafa30045@gmail.com>
2021-09-18 22:41:13 +0300

    initial commit

---
+++ src/main/java/com/test6/test6/controller/testController.java
@@ -0,0 +1,20 @@
+package com.test6.test6.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test6")
+public class testController {
+
+    @GetMapping("/getexample")
+    public ResponseEntity<String> example()
+    {
+        return ResponseEntity.ok("sad");
+
+    }
+}
