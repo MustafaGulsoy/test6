a217c878f5b60c64b78708bbc8a4ad5eab576df6
Mustafa Gülsoy <mustafa30045@gmail.com>
2021-09-18 23:56:14 +0300

    commit 1

--- src/main/java/com/test6/test6/controller/testController.java
+++ src/main/java/com/test6/test6/controller/testController.java
@@ -14,7 +14,7 @@ public class testController {
     @GetMapping("/getexample")
     public ResponseEntity<String> example()
     {
-        return ResponseEntity.ok("sdsdsdaasd");
+        return ResponseEntity.ok("sd");
 
     }
 }
